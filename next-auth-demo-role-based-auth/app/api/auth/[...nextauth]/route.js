f8c4b405d7d7e4ae44aeac7c1bf7e9843c399a55
Siyanda Mkhize <siyandam097@gmail.com>
2023-10-01 10:13:56 +0200

    well

---
+++ next-auth-demo-role-based-auth/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,51 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from '@/lib/mongo/client'
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          role: profile.role ?? 'user'
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user, trigger, session }) {
+      if (user) {
+        token.role = user.role
+      }
+
+      if (trigger === 'update' && session?.name) {
+        token.name = session.name
+      }
+
+      return token
+    },
+    async session({ session, token }) {
+      session.user.role = token.role
+      return session
+    }
+  },
+  pages: {
+    signIn: '/signin'
+  },
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: 'jwt'
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
---
+++ notes.js
@@ -0,0 +1,15 @@
+//portfolio 
+//  sections 
+//      Landing page
+//      A few case studies
+//      Work / School history
+
+//      Skills
+//          User reaserch
+//          User Flows
+//          Wireftraming
+//          Mockups
+//          persona development
+//          Visual design
+//          information architecture
+//          Agile design and collaboration
